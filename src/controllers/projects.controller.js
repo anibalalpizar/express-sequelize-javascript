fbc78d1d737f1dde2880aa33f4a239b7cb00d044
anibalalpizar <anibal.alpizar14@gmail.com>
2024-10-09 22:34:35 -0600

    Add update, delete, and get by ID functions to project controller and update routes

--- src/controllers/projects.controller.js
+++ src/controllers/projects.controller.js
@@ -1,6 +1,5 @@
 import { Project } from '../models/Project.js';
 
-
 export const getProjects = async (req, res) => {
     try {
         const projects = await Project.findAll();
@@ -16,13 +15,64 @@ export const createProject = async (req, res) => {
     const { name, priority, description } = req.body;
 
     try {
-        const newProject = await Project.create({
-            name, priority, description
-        })
+        const newProject = await Project.create({ name, priority, description })
         res.json(newProject);
     } catch (error) {
         res.status(500).json({
             message: "Error creating project"
         })
     }
+}
+
+export const updateProject = async (req, res) => {
+    const { id } = req.params;
+    const { name, priority, description } = req.body;
+
+    try {
+        const project = await Project.findByPk(id);
+        project.name = name;
+        project.priority = priority;
+        project.description = description;
+
+        await project.save();
+
+        res.json(project);
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Error updating project"
+        })
+    }
+}
+
+export const deleteProject = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        await Project.destroy({ where: { id } });
+        res.sendStatus(204);
+    } catch (error) {
+        res.status(500).json({
+            message: "Error deleting project"
+        })
+    }
+}
+
+export const getProjectById = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const project = await Project.findOne({ where: { id } });
+
+        if (!project) return res.status(404).json({
+            message: "Project not found"
+        })
+        
+        res.json(project);
+    } catch (error) {
+        res.status(500).json({
+            message: "Error retrieving project"
+        })
+    }
+
 }
