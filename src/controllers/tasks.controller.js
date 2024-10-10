a3ad9d6d53a8576f6995a3447c6cd9662fe0243e
anibalalpizar <anibal.alpizar14@gmail.com>
2024-10-09 23:39:21 -0600

    Add task retrieval, update, and deletion functions to tasks controller and routes

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -3,6 +3,10 @@ import { Tasks } from "../models/Task.js";
 export const getTasks = async (req, res) => {
     try {
         const tasks = await Tasks.findAll();
+        if (tasks.length === 0)
+            return res.status(404).json({
+                message: "No tasks found"
+            })
         res.json(tasks)
     } catch (error) {
         res.status(500).json({
@@ -27,3 +31,50 @@ export const createTask = async (req, res) => {
         })
     }
 };
+
+export const getTaskById = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const task = await Tasks.findOne({ where: { id } });
+
+        if (!task)
+            return res.status(404).json({
+                message: "Task not found"
+            })
+
+        res.json(task)
+    } catch (error) {
+        res.status(500).json({
+            message: "Error retrieving task"
+        })
+    }
+}
+
+export const updateTask = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const task = await Tasks.findOne({ where: { id } });
+        task.set(req.body);
+        await task.save();
+        res.json(task);
+    } catch (error) {
+        res.status(500).json({
+            message: "Error updating task"
+        })
+    }
+}
+
+export const deleteTask = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        await Tasks.destroy({ where: { id } });
+        res.sendStatus(204)
+    } catch (error) {
+        res.status(500).json({
+            message: "Error deleting task"
+        })
+    }
+}
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { getTasks, createTask } from "../controllers/tasks.controller.js";
+import { getTasks, createTask, updateTask, deleteTask, getTaskById } from "../controllers/tasks.controller.js";
 
 const router = Router();
 
 router.get('/tasks', getTasks)
 router.post('/tasks', createTask)
-router.put('/tasks/:id')
-router.delete('/tasks/:id')
-router.get('/tasks/:id')
+router.put('/tasks/:id', updateTask)
+router.delete('/tasks/:id', deleteTask)
+router.get('/tasks/:id', getTaskById)
 
 export default router;
