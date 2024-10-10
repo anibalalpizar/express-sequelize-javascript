20dc7199b784853b12de4b85bf885056a235e806
anibalalpizar <anibal.alpizar14@gmail.com>
2024-10-09 21:39:47 -0600

    Add projects router and configure JSON parsing in Express app

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 import express from 'express';
+import projectsRouter from './routes/projects.routes.js';
 
 const app = express();
+
+app.use(express.json());
+
+app.use(projectsRouter);
 
 export default app;
