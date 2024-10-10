bf00a1133ada5fef441f698122e5c5e9b8e3f8ae
anibalalpizar <anibal.alpizar14@gmail.com>
2024-10-09 23:26:05 -0600

    Add tasks router and integrate with main app

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import projectsRouter from './routes/projects.routes.js';
+import tasksRoutes from './routes/tasks.routes.js';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(projectsRouter);
+app.use(tasksRoutes);
 
 export default app;
